d034a73d98036076f79f39c4bb7de166ec29fb73
bartoszb0 <bartoszb2020@gmail.com>
2025-04-29 23:30:43 +0200

    Added editing the quantity

--- script.js
+++ script.js
@@ -51,21 +51,71 @@ function addItem(e) {
             newRow = table.insertRow()
             newRow.insertCell().innerHTML = `${basket.length}`;
             newRow.insertCell().innerHTML = name;
-            newRow.insertCell().innerHTML = quantity;
-            newRow.insertCell().innerHTML = `${product.price}zł`;
-            newRow.insertCell().innerHTML = `${price.toFixed(2)}zł`;
 
-            updateSum(price);
+            const shownQuantity = newRow.insertCell();
+            shownQuantity.innerHTML = quantity;
+            shownQuantity.setAttribute('name', 'productQuantity');
+            shownQuantity.addEventListener('click', editQuantity);
+
+            const shownPrice = newRow.insertCell();
+            shownPrice.innerHTML = `${product.price}zł`;
+            shownPrice.setAttribute('name', 'productPrice');
+            
+            const shownSum = newRow.insertCell();
+            shownSum.innerHTML = `${price.toFixed(2)}zł`;
+            shownSum.setAttribute('name', 'productSum');
+
+            updateSum();
         }
     })
     this.reset();
 }
 
-function updateSum(price=null) {
-    if (price === null) {
-        sumPrice.innerHTML = '0zł'
+function editQuantity(e) {
+    const oldValue = e.target.innerHTML
+    const input = document.createElement('input');
+    input.value = oldValue
+    input.classList.add('quantityControl')
+    e.target.replaceWith(input);
+    input.addEventListener('keyup', (e) => {
+        if (e.key === 'Enter' && validateNewQ(input.value)) {
+            this.innerHTML = input.value;
+            input.replaceWith(this);
+            updatePrice(this);
+        }
+    })
+}
+
+function validateNewQ(input) {
+    if (!parseFloat(input) || (input % 0.5)) {
+        return false
     } else {
-        sumPrice.innerHTML = `${(parseFloat(sumPrice.innerHTML) + price).toFixed(2)}zł`;
+        return true
+    }
+}
+
+function updatePrice(element) {
+    const row = element.parentElement
+    
+    const quantity = parseFloat(row.querySelector('[name=productQuantity]').innerHTML);
+    const price = parseFloat(row.querySelector('[name=productPrice]').innerHTML);
+
+    // Change the sum of current price
+    const newPrice = parseFloat(quantity * price)
+    row.querySelector('[name=productSum]').innerHTML = `${newPrice.toFixed(2)}zł`
+
+    // Update the whole sum price
+    updateSum();
+}
+
+function updateSum() {
+    const allPrices = [...document.querySelectorAll('[name=productSum]')];
+    if (allPrices.length === 0) {
+        sumPrice.innerHTML = `0zł`;
+    } else {
+        let newSum = 0;
+        allPrices.forEach(price => newSum += parseFloat(price.innerHTML))
+        sumPrice.innerHTML = `${newSum.toFixed(2)}zł`;
     }
 }
 
